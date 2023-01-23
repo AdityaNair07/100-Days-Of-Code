e2624cf1a833e778e2b50c475f1c7f66cb6bebff
AdityaNair07 <96580997+AdityaNair07@users.noreply.github.com>
2023-01-23 19:38:36 +0530

    CatsAndMouse

---
+++ Day 77/CatsAndMouse.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+class CatsAndMouse {
+    static String catAndMouse(int x, int y, int z) {
+        String result = "";
+        if (Math.abs(z - x) > Math.abs(z - y)) {
+            result = "Cat B";
+        } else if (Math.abs(z - x) < Math.abs(z - y)) {
+            result = "Cat A";
+        } else {
+            result = "Mouse C";
+        }
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < q; i++) {
+            String[] xyz = scanner.nextLine().split(" ");
+
+            int x = Integer.parseInt(xyz[0]);
+
+            int y = Integer.parseInt(xyz[1]);
+
+            int z = Integer.parseInt(xyz[2]);
+
+            String result = catAndMouse(x, y, z);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
---
+++ Day 77/Readme.md
@@ -0,0 +1,19 @@
+<h1 align="center"></h1>
+
+- First created the main class CatsAndMouse.
+- Inside the class defined a static String return type method catAndMouse() that takes integer variables x,y and z as parameters.
+- Inside the method declared an empty String variable result.
+- Then defined an if-else condition.
+- If the absolute difference between z and x is greater than the absolute difference between z and y, then result stores Cat B as String value.
+- Else if the absolute difference between z and x is smaller than the absolute difference between z and y, then result stores Cat A as String value.
+- Else result stores Mouse C as String value.
+- The method returns result.
+- Declared private static final object of Scanner scanner that takes user input.
+- Inside the main method created an object of BufferedWriter bw that provides output.
+- Declared an integer variable q that takes Integer user input.
+- Defined a for loop that runs from 0 to q.
+- Inside the loop declared String array xyz that stores user input.
+- Declared three integer variables x,y and z that stores Integer values at index 0, 1 and 2.
+- Declared a String result that stores catAndMouse() output that takes x,y and z as arguments.
+- Finally printing result value using write().
+- Hence providing output.
