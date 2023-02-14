0f186eb863811fcec917c9464bb254ed404d2e9d
AdityaNair07 <96580997+AdityaNair07@users.noreply.github.com>
2023-02-14 19:27:56 +0530

    String Construction

---
+++ Day 99/Readme.md
@@ -0,0 +1,13 @@
+<h1 align="center">String Construction</h1>
+
+- First created a class Result.
+- Inside the class defined a static integer return type method stringConstruction() that takes String s as parameter.
+- Inside the method declared an integer variable count that is initialised to 0.
+- Then declared an integer array a of size 26.
+- Then defined a for loop that runs from 0 to length of s.
+- Inside the loop incrementing the value at index, character at ith index - 'a' of a array.
+- Then defined another for loop that runs from 0 to length of a.
+- Inside the loop defined an if condition that checks if ith element of a is not equal to 0.
+- If the condition is satisfied then count value is incremented.
+- The method returns count value.
+- Hence providing the output.
---
+++ Day 99/StringConstruction.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.util.stream.*;
+
+class Result {
+    public static int stringConstruction(String s) {
+        int count = 0;
+        int a[] = new int[26];
+        for (int i = 0; i < s.length(); i++) {
+            a[s.charAt(i) - 'a']++;
+        }
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] != 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+}
+
+class StringConstruction {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s = bufferedReader.readLine();
+
+                int result = Result.stringConstruction(s);
+
+                bufferedWriter.write(String.valueOf(result));
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
